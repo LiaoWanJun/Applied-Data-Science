28a38893f05d19be625b7184c3551fdeb10535f2
LiaoWanJun <71477819+LiaoWanJun@users.noreply.github.com>
2020-12-11 11:53:22 +0800

    Add files via upload

---
+++ A107260207-廖婉君-SHU-R-Midterm-source.R
@@ -0,0 +1,56 @@
+# 2020/12/11(五), 109學年第一學期 資料科學應用 R期中考
+
+#學號:A107260207        姓名:廖婉君 
+  
+  # (請依照規定)貼上執行程式碼及執行結果。
+
+# ex1
+study <- function(d){
+  # 補習英文 X 小時
+  # 補習電腦 Y 小時
+   U = (X^1/2, Y^1/2)
+   if Tuition <= 12000, Fit(*)
+}
+
+
+
+# ex2(a)
+scoreA <- read_excel("exam/Score-109.xlsx", skip=1)
+head(scoreA, 5)
+tail(scoreA, 5)
+str(scoreA)
+
+# ex2(b)
+set.seed(12345) 
+scoreA <- c(NA, 0:100) 
+English <- sample(scoreA, 50, replace=T) 
+Calculus <- sample(scoreA, 50, replace=T)
+
+# ex2(c)
+my.cor <- function(x){
+ # x <- English
+ # y <- Calculus
+  cor
+}
+
+# ex2(d)
+my.cor <- function(X){
+  # x <- English
+  # y <- Calculus
+  cor
+}
+
+cor
+
+# ex3(a)
+my.dnorm <-function(X){
+  # x <- 一個數值
+  mean = 0, sd=1,log = FALSE
+  cat(C_dnorm, x, mean, sd, log = FALSE)
+}
+
+# ex3(b)
+str(dnorm)
+dnorm
+
+
